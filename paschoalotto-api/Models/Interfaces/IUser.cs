a34f256d7ed06d18b1f48af117c759dbcb99bcef
Pedro Hugo Heinen <pedro.hugo.heinen@live.com>
2025-01-03 22:25:19 -0300

    Created class User and generated migration

---
+++ paschoalotto-api/Models/Interfaces/IUser.cs
@@ -0,0 +1,19 @@
+﻿namespace paschoalotto_api.Models.Interfaces
+{
+    public interface IUser : IBaseEntity
+    {
+        string FirstName { get; set; }
+        
+        string LastName { get; set; }
+        
+        string Email { get; set; }
+
+        string Phone { get; set; }
+
+        string Gender { get; set; }
+
+        int Age { get; set; }
+
+        bool Status { get; set; }
+    }
+}
