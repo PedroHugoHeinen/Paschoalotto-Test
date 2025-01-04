be32c4a28aa5d01881089ae6271f4bc20944706e
Pedro Hugo Heinen <pedro.hugo.heinen@live.com>
2025-01-04 00:30:33 -0300

    Created endpoint UserController

---
+++ paschoalotto-api/Controllers/UserController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using paschoalotto_api.Models;
+using paschoalotto_api.Data;
+using Microsoft.EntityFrameworkCore;
+using paschoalotto_api.Globals.DTOs;
+using paschoalotto_api.Services.Interfaces;
+
+namespace paschoalotto_api.Controllers
+{
+    [ApiController]
+    [Route("api/user")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService userService;
+
+        public UsersController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<ActionResult<IEnumerable<UserDTO>>> GetAllAsync()
+        {
+            var response = await this.userService.GetAllAsync();
+
+            if (response == default)
+            {
+                return NotFound();
+            }
+
+            return Ok(response);
+        }
+
+        [HttpGet("GetById/{id}")]
+        public async Task<ActionResult<UserDTO>> GetByIdAsync(int id)
+        {
+            var response = await this.userService.GetByIdAsync(id);
+
+            if (response == default)
+            {
+                return NotFound();
+            }
+
+            return Ok(response);
+        }
+
+        [HttpPost("Insert")]
+        public async Task<ActionResult<UserDTO>> InsertAsync(UserDTO userDTO)
+        {
+            var response = await this.userService.InsertAsync(userDTO);
+
+            return response;
+        }
+        
+        [HttpPut("Update/{id}")]
+        public async Task<ActionResult<UserDTO>> UpdateAsync(UserDTO userDTO)
+        {
+            var response = await this.userService.UpdateAsync(userDTO);
+
+            if (response == default)
+            {
+                return NotFound();
+            }
+
+            return response;
+        }
+
+        [HttpDelete("Delete/{id}")]
+        public async Task<ActionResult<bool>> DeleteAsync(int id)
+        {
+            var response = await this.userService.DeleteAsync(id);
+
+            if (!response)
+            {
+                return NotFound();
+            }
+
+            return response;
+        }
+    }
+}
+
+
+
+
+        
+
---
+++ paschoalotto-api/Globals/DTOs/BaseDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace paschoalotto_api.Globals.DTOs
+{
+    public class BaseDTO
+    {
+        public int Id { get; set; }
+
+        public DateTimeOffset CreateAt { get; set; }
+
+        public DateTimeOffset? LastUpdateAt { get; set; }
+    }
+}
